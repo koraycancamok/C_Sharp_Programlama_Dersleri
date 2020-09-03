f0ef1675cb31599e6127847fbc214d06b1fd9e69
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:26:20 +0300

    AssignmentOperators

---
+++ Variables/AssignmentOperators/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AssignmentOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 40;
+            var b = 20;
+            a += b;
+            var c = a + b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
