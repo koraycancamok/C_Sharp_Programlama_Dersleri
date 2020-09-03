bf35fa2979eb8a3158e38c2faaec22e24ea1366c
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 11:50:28 +0300

    ControlFlow_Switch

---
+++ ControlFlow_Switch/ControlFlow_Switch/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace ControlFlow_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int gun = (int)DateTime.Now.DayOfWeek;
+            //switch (gun)
+            //{
+            //    case 1:
+            //        Console.WriteLine("bugün pazartesi");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("bugün salı");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("bugün çarşamba");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("bugün perşembe");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("bugün cuma");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("bugün cumartesi");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("bugün pazar");
+            //        break;
+            //    default:
+            //        Console.WriteLine("hatalı bir gün");
+            //        break;
+            //}
+
+            //Console.Write("ay giriniz : ");
+            //int ay = int.Parse(Console.ReadLine());
+            //switch (ay)
+            //{
+            //    case 12:
+            //    case 1:
+            //    case 2:
+            //        Console.WriteLine("kış mevsimindeyiz");
+            //        break;
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        Console.WriteLine("ilkbahar mevsimindeyiz");
+            //        break;
+            //    case 6:
+            //    case 7:
+            //    case 8:
+            //        Console.WriteLine("yaz mevsimindeyiz");
+            //        break;
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        Console.WriteLine("sonbahar mevsimindeyiz");
+            //        break;
+            //    default:
+            //        Console.WriteLine("hatalı ay girdiniz");
+            //        break;
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
