25e987bbe9840997cb1e6c698ceb206e09fd6faf
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 11:56:26 +0300

    ForLoop

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 100;
+            //for (; i >= 10;)
+            //{
+            //    Console.WriteLine(i);
+            //    i = i - 2;
+            //}
+
+            //int toplamcift = 0;
+            //int toplamtek = 0;
+            //for (int i = 1; i < 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        toplamcift += i;
+            //    }
+            //    else
+            //    {
+            //        toplamtek += i;
+            //    }
+            //}
+            //Console.WriteLine("Tek sayıların toplamı : {0}", toplamtek);
+            //Console.WriteLine("Çift sayıların toplamı : {0}", toplamcift);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ForLoop/ForLoop2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ForLoop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("başlangıc değeri : ");
+            int baslangic = int.Parse(Console.ReadLine());
+            Console.Write("bitiş değeri : ");
+            int bitis = int.Parse(Console.ReadLine());
+            Console.Write("artış miktarı : ");
+            int artis = int.Parse(Console.ReadLine());
+            int sayac = 0;
+            int toplam = 0;
+            int carpım = 1;
+            int karesi = 1;
+            for (int i = baslangic; i <= bitis; i = i + artis)
+            {
+                sayac++;
+                toplam = toplam + i;
+                carpım = carpım * i;
+                karesi = i * i;
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("{0}. sayı : {1} sayı çift ve karesi {2}", sayac, i, karesi);
+                }
+                else
+                {
+                    Console.WriteLine("{0}. sayı : {1} sayı tek ve karesi {2}", sayac, i, karesi);
+                }                           
+            }
+            Console.WriteLine("toplam değer : {0}",toplam);
+            Console.WriteLine("çarpım değer : {0}",carpım);
+
+            Console.ReadLine();
+        }
+    }
+}
