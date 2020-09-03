662ce7c042fe931ba19e93157935fecabdbbd367
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:24:13 +0300

    AritmeticOperators

---
+++ Variables/AritmeticOperators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AritmeticOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 15;
+            var b = 4;
+            var c = 3;
+
+            Console.WriteLine(a+b);
+            Console.WriteLine(a-b);
+            Console.WriteLine(a*b);
+            Console.WriteLine(a/b);
+            Console.WriteLine((float)a/(float)b);
+            Console.WriteLine((a+b)*c);
+            Console.WriteLine(a%2);
+            Console.WriteLine(b%2);
+            Console.WriteLine(c%2);
+            Console.WriteLine(Math.Pow(2,3));
+            Console.WriteLine(a++);
+            Console.WriteLine(a);
+            Console.WriteLine(b--);
+            Console.WriteLine(b);
+            Console.WriteLine(c=b++);
+
+            int d = 3, e = 5;
+
+            Console.WriteLine(d=++e);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+
+            Console.ReadLine();
+        }
+    }
+}
