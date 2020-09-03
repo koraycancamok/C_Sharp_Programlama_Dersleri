71676859ff313bcd796a6661bd1ce331ac2c44b2
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:32:05 +0300

    TypeConversion

---
+++ Variables/TypeConversion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                try
+                {
+                    string sayi = "256,50";
+                    double i = double.Parse(sayi);
+                    //long i = Convert.ToInt64(sayi);
+                    //double i = Convert.ToDouble(sayi);
+                    //string sayi = "true";
+                    //bool i = bool.Parse(sayi);
+                    //bool i = Convert.ToBoolean(sayi);
+                    Console.WriteLine(i);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Tür dönüşüm hatası");
+                }                   
+            Console.ReadLine();
+        }
+    }
+}
