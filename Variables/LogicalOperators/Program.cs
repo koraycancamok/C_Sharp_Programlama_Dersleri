308cd6fb33ad138b96bc6404970e1e878d341d5d
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:29:21 +0300

    LogicalOperators

---
+++ Variables/LogicalOperators/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = true;
+            var b = true;
+            var c = false;
+            var d = false;
+
+            Console.WriteLine("&");
+            Console.WriteLine(a & b);
+            Console.WriteLine(a & c);
+            Console.WriteLine(c & d);
+            Console.WriteLine("-----");
+
+            Console.WriteLine("|");
+            Console.WriteLine(a | b);
+            Console.WriteLine(a | c);
+            Console.WriteLine(c | d);
+            Console.WriteLine("-----");
+
+            Console.WriteLine("^");
+            Console.WriteLine(a ^ b);
+            Console.WriteLine(a ^ c);
+            Console.WriteLine(c ^ d);
+            Console.WriteLine("-----");
+
+            Console.WriteLine("&&");
+            Console.WriteLine(a && b);
+            Console.WriteLine(a && c);
+            Console.WriteLine(c && d);
+            Console.WriteLine("-----");
+
+            Console.WriteLine("||");
+            Console.WriteLine(a || b);
+            Console.WriteLine(a || c);
+            Console.WriteLine(c || d);
+            Console.WriteLine("-----");
+
+            Console.WriteLine("!");
+            Console.WriteLine(!a);
+            Console.WriteLine("-----");
+
+            Console.WriteLine(!(((a & b) || c) & d));
+
+            Console.ReadLine();
+        }
+    }
+}
