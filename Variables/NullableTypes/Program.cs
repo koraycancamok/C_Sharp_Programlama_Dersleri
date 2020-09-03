140832a982446092f8a05ad0e64ad48bcb152b1e
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:31:17 +0300

    NullableTypes

---
+++ Variables/NullableTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool? sonuc = null;
+            //int? sayi = null;
+            //sayi = 100;
+            //if (sayi.HasValue == true)
+            //{
+            //    Console.WriteLine(sayi);
+            //}
+            //Console.WriteLine(sayi.HasValue);
+
+            //int? x = 10;
+            //int? y = 20;
+            //Console.WriteLine("X : {0}, Y : {1} ",x,y);
+
+            //x = y.GetValueOrDefault();
+            //Console.WriteLine("X : {0}, Y : {1} ", x, y);
+
+            //x = null;
+            //y = x.GetValueOrDefault();
+            //Console.WriteLine("X : {0}, Y : {1} ", x, y);
+
+            //y = x.GetValueOrDefault(10);
+            //Console.WriteLine("X : {0}, Y : {1} ", x, y);
+
+            //y = x ?? 20;
+            //Console.WriteLine("X : {0}, Y : {1} ", x, y);
+
+            //x = 25;
+            //y = x ?? 20;
+            //Console.WriteLine("X : {0}, Y : {1} ", x, y);
+
+            //Console.ReadLine();
+        }
+    }
+}
