0c34facfa1352655f5c5794a7636fb3d95144f8b
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:07:56 +0300

    StringUygulama

---
+++ StringUygulama/StringUygulama/HelperMethod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringUygulama
+{
+    public class HelperMethod
+    {
+        public static string Ozetle(string str, int maxKarakter = 30)
+        {
+            if (maxKarakter > str.Length)
+                return str;
+
+            //var ozet = cumle.Substring(0, maxKarakter) + "...";
+            //Console.WriteLine(ozet);
+            var kelimeler = str.Split(' ');
+            var toplamkarakter = 0;
+            var ozetkelimeler = new List<string>();
+            foreach (var kelime in kelimeler)
+            {
+                ozetkelimeler.Add(kelime);
+                toplamkarakter += kelime.Length;
+                if (toplamkarakter > maxKarakter)
+                    break;
+            }
+            return string.Join(" ", ozetkelimeler) + "...";
+        }
+    }
+}
---
+++ StringUygulama/StringUygulama/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cumle = "Eğer bir cümlenin toplam karakter sayısı belirtilen bir karakter sayısıdan fazla ise cümleyi kesmemiz gerekiyor.";
+            var ozet = HelperMethod.Ozetle(cumle,20);
+            Console.WriteLine(ozet);
+        }
+    }
+}
