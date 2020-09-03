3c7882ffd714c75287b88e5ffaaf4c1c252698e1
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:12:56 +0300

    UsingDirectory

---
+++ UsingDirectory/UsingDirectory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace UsingDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\altklasor";
+            var path2 = @"C:\deneme";
+            var path3 = @"C:\deneme1";
+            //Console.WriteLine("şu an çalışılan klasör : {0}",Directory.GetCurrentDirectory());
+            //Console.WriteLine("c sürücüsündeki klasörler");
+            //Console.WriteLine("--------------");
+            //string[] klasorler = Directory.GetDirectories(path);
+            //foreach (var item in klasorler)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //if (!Directory.Exists(path2))
+            //{
+            //    Directory.CreateDirectory(path2);
+            //}
+            //Directory.Move(path2, path3);
+            //Directory.Delete(path3);
+
+            DirectoryInfo directoryinfo = new DirectoryInfo(path);
+            //directoryinfo.CreateSubdirectory("altklasor");
+            if (directoryinfo.Exists)
+            {
+                directoryinfo.Delete();
+            }
+        }
+    }
+}
