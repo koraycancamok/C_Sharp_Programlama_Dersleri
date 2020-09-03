8c1a9c74f17bee5ae352d06ab0f84568b6384509
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:16:02 +0300

    UsingPath

---
+++ UsingPath/UsingPath/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace UsingPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\drive";
+            string[]  files = Directory.GetFiles(path);
+            foreach (var item in files)
+            {
+                Console.WriteLine("dosya adı : {0}",Path.GetFileName(item));
+                Console.WriteLine("dosya uzantısı : {0}",Path.GetExtension(item));
+                Console.WriteLine("sadece dosya adı : {0}",Path.GetFileNameWithoutExtension(item));
+                Console.WriteLine("üst klasör : {0}",Path.GetDirectoryName(item));
+            }
+            var filename = Path.GetRandomFileName();
+            Console.WriteLine(filename);
+        }
+    }
+}
