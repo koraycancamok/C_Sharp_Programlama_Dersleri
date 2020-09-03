34e48568c538ef7af82f0e9d27c8efc7441fb1e8
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 11:49:17 +0300

    ControlFlow_If

---
+++ ControlFlow_If/ControlFlow_If/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ControlFlow_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.Write("bir sayı giriniz : ");
+            var sayi = int.Parse(Console.ReadLine());
+            if (sayi == 0)
+            {
+                Console.WriteLine("sayı sıfıra eşit.");
+            }
+            else if (sayi > 0)
+            {
+                Console.WriteLine("sayı pozitif");
+            }
+            else
+            {
+                Console.WriteLine("sayı negatif");
+            }
+            */
+
+            /*
+            Console.Write("yaşınızı giriniz : ");
+            var yas = int.Parse(Console.ReadLine());
+            if (yas >= 18)
+            {
+                Console.Write("öğrenim durumunuz : ");
+                string ogrenim = Console.ReadLine();
+                if (ogrenim == "lise" || ogrenim == "üniversite")
+                {
+                    Console.WriteLine("ehliyet alabilirsiniz");
+                }
+                else
+                {
+                    Console.WriteLine("öğrenim durumunuz ehliyet almak için yeterli değil");
+                }
+            }
+            else
+            {
+                Console.WriteLine("ehliyet alamazsınız, {0} sene sonra alabilirsiniz",18-yas);
+            }
+            */
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ControlFlow_If/ControlFlow_If2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ControlFlow_If2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Toplama için : + ");
+            Console.WriteLine("Çıkarma için : - ");
+            Console.WriteLine("Çarpma için : * ");
+            Console.WriteLine("Bölme için : / ");
+            double sonuc = 0;
+            bool ok = true;
+            bool bolmehata = true;
+            Console.Write("Seçiminiz : ");
+            string secim = Console.ReadLine();
+            Console.Write("1.sayı : ");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.Write("2.sayı : ");
+            int sayi2 = int.Parse(Console.ReadLine());
+            if (secim == "+")
+            {
+                sonuc = sayi1 + sayi2;
+            }
+            else if (secim == "-")
+            {
+                sonuc = sayi1 - sayi2;
+            }
+            else if (secim == "*")
+            {
+                sonuc = sayi1 * sayi2;
+            }
+            else if (secim == "/")
+            {
+                if (sayi2 == 0)
+                {
+                    Console.WriteLine("ikini sayı 0 olduğundan dolayı bölme işlemi gerçekleştirilemedi");
+                }
+                else
+                {
+                    sonuc = sayi1 / sayi2;
+                }
+            }
+            else
+            {
+                ok = false;
+                Console.WriteLine("Hatalı bir işlem tipi seçtiniz.");
+            }
+            if (ok && bolmehata == false)
+            {
+                Console.WriteLine("İşlem sonucu : {0} {1} {2} = {3}", sayi1, secim, sayi2, sonuc);
+            }
+        }
+    }
+}
