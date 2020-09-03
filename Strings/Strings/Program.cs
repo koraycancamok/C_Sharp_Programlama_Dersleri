48aa59fcbcda14c84c325c7f2f0b6acc1151b723
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:07:10 +0300

    Strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ad = "koray";
+            var soyad = "çamok";
+            //var adsoyad = "adınız : " + ad + ", " + "soyadınız : " + soyad;
+            //var adsoyad = string.Format("adınız : {0}, soyadınız : {1}", ad, soyad);
+            var adsoyad = $"adınız : {ad}, soyadınız : {soyad}";
+            Console.WriteLine(adsoyad);
+            Console.WriteLine("-------------");
+            string[] adlar = {"ali", "koray", "ahmet", null};
+            if (string.IsNullOrEmpty(adlar[3]))
+            {
+                Console.WriteLine("isim boş");
+            }
+            var tumadlar = string.Join("-",adlar);
+            Console.WriteLine(tumadlar);     
+        }
+    }
+}
