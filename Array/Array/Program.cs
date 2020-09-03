b3d8d4dac22bc597b90bf691dcbd29f2f853383e
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 11:39:22 +0300

    Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        class Ogrenci
+        {
+            public string Ad;
+            public int No; 
+        }
+        static void Main(string[] args)
+        {
+            //int[] ogrno = new int[5];
+            //ogrno[0] = 100;
+            //ogrno[1] = 200;
+            //ogrno[2] = 300;
+            //ogrno[3] = 400;
+            //ogrno[4] = 500;
+            //for (int i = 0; i < ogrno.Length; i++)
+            //{
+            //    Console.WriteLine(ogrno[i]);
+            //}
+            //Console.WriteLine("-------");
+            //foreach (var ogr in ogrno)
+            //{
+            //    Console.WriteLine(ogr);
+            //}
+
+            //int[] ogrno = new int[5];
+            //ogrno[0] = 100;
+            //ogrno[1] = 200;
+            //ogrno[2] = 300;
+            //ogrno[3] = 400;
+            //ogrno[4] = 500;
+            //string[] ogrenciler = new string[5];
+            //ogrenciler[0] = "ahmet";
+            //ogrenciler[1] = "koray";
+            //ogrenciler[2] = "bengisu";
+            //ogrenciler[3] = "serpil";
+            //ogrenciler[4] = "murat";
+            //foreach (var ogr in ogrenciler)
+            //{
+            //    Console.WriteLine(ogr);
+            //}
+
+            //Ogrenci[] ogrenciler = new Ogrenci[5];
+            //ogrenciler[0] = new Ogrenci() { Ad = "ahmet", No = 100 };
+            //ogrenciler[1] = new Ogrenci() { Ad = "koray", No = 200 };
+            //ogrenciler[2] = new Ogrenci() { Ad = "bengisu", No = 300 };
+            //ogrenciler[3] = new Ogrenci() { Ad = "serpil", No = 400 };
+            //ogrenciler[4] = new Ogrenci() { Ad = "murat", No = 500 };
+            //foreach (var ogr in ogrenciler)
+            //{
+            //    Console.WriteLine("Öğrenci adı : {0}, Öğrenci No : {1}", ogr.Ad, ogr.No);
+            //}
+            //Console.WriteLine("------------");
+            //for (int i = 0; i < ogrenciler.Length; i++)
+            //{
+            //    Console.WriteLine("Öğrenci adı : {0}, Öğrenci No : {1}", ogrenciler[i].Ad, ogrenciler[i].No);
+            //}
+        }
+    }
+}
