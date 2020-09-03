b59fbd81bf5a35f4862a5db41f7e07d172389817
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:25:16 +0300

    AritmeticOperators2

---
+++ Variables/AritmeticOperators2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AritmeticOperators2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10, b = 20, c = 50;
+            //Console.WriteLine((a+b)*c);
+
+            //int? a = null;
+            //int b = 100;
+            //int sonuc = (a ?? 0) + b;
+            //Console.WriteLine(sonuc);
+
+            //int a = 10;
+            //int b = 50;
+            //Console.WriteLine(a=b--);
+            //Console.WriteLine(b);
+
+            //int a = 10, b = 20, c = 50;
+            //int sonuc = a + (b++) - c;
+            //Console.WriteLine(sonuc);
+
+            //int a = 10;
+            //int b = 10;
+            //Console.WriteLine(a==b);
+
+            //double sonuc = Math.Pow(3, 5);
+            //Console.WriteLine(sonuc);
+
+            //Console.Write("bir sayı giriniz : ");
+            //int sayi = int.Parse(Console.ReadLine());
+            //if (sayi % 2 == 0)
+            //{
+            //    Console.WriteLine("sayı çift sayı.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("sayı tek sayı.");
+            //}
+
+            //Console.ReadLine();
+        }
+    }
+}
