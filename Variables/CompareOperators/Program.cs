93073b678e44a5b690c14d3e8c75b6b533f48cec
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:27:43 +0300

    CompareOperators

---
+++ Variables/CompareOperators/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CompareOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 10;
+            var c = 25;
+            var d = 30;
+
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != c);
+            Console.WriteLine(c > b);
+            Console.WriteLine(c < b);
+            Console.WriteLine(a >= b);
+            Console.WriteLine(a <= b);
+
+            var sonuc = (c > b) ?
+                                 (c > d) ?
+                                          "c d'den büyük"
+                                          :
+                                          "c d'den küçük"
+                                 :
+                                 "b c'den büyük";
+
+            Console.WriteLine(sonuc);
+
+
+            Console.ReadLine();
+        }
+    }
+}
