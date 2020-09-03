bd58c7a1ece614244b783873543cb3fca78efe61
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 11:52:53 +0300

    doWhile

---
+++ doWhile/doWhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace doWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "";
+            string password = "";
+            byte hak = 2;
+            do
+            { 
+                if (hak == 0)
+                {
+                    break;
+                }
+                Console.Write("kullanıcı adı : ");
+                username = Console.ReadLine();
+                Console.Write("parola : ");
+                password = Console.ReadLine();
+                hak--;
+            } while (username != "koray" || password != "12345");
+            if (hak == 0)
+            {
+                Console.WriteLine("hakkınız bitti");
+            }
+            else
+            {
+                Console.WriteLine("giriş başarılı");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ doWhile/doWhile2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace doWhile2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secim;
+            double bakiye = 0;
+            do
+            {
+                Console.Write("1-bakiye görüntüleme \n 2-para yatırma \n 3-para çekme \n 4-çıkış \n");
+                Console.Write("seçiminiz : ");
+                secim = Console.ReadLine();
+                Console.WriteLine("--------------------------------------------------");
+                switch (secim)
+                {
+                    case "1":
+                        Console.WriteLine("bakiyeniz : {0} TL", bakiye);
+                        if (bakiye < 500)
+                        {
+                            Console.WriteLine("bakiyeniz 500 TL'nin altına düşmüştür.");
+                        }
+                        Console.WriteLine("--------------------------------------------------");
+                        break;
+                    case "2":
+                        Console.Write("ne kadar para yatırmak istiyorsunuz : ");
+                        double miktar = double.Parse(Console.ReadLine());
+                        Console.WriteLine("--------------------------------------------------");
+                        bakiye += miktar;
+                        break;
+                    case "3":
+                        Console.Write("ne kadar para çekmek istiyorsunuz : ");
+                        double eksilen = double.Parse(Console.ReadLine());
+                        Console.WriteLine("--------------------------------------------------");
+                        if (eksilen > bakiye)
+                        {
+                            Console.WriteLine("bakiye yetersiz");
+                            Console.WriteLine("--------------------------------------------------");
+                        }
+                        else
+                        {
+                            bakiye -= eksilen;
+                        }
+                        break;
+                }
+            } while (secim != "4");
+            
+            Console.ReadLine();
+        }
+    }
+}
