f40459b2fe4525addb74064164a383110cd24426
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:10:29 +0300

    Ternary

---
+++ Ternary/Ternary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("bir sayı giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+
+            var sonuc = (sayi % 2 == 0) ?
+                (sayi > 0 ? "sayı çift pozitif" : "sayı çift negatif") :
+                (sayi > 0 ? "sayı tek pozitif" : "sayı tek negatif");
+            Console.WriteLine(sonuc);
+
+            Console.ReadLine();
+        }
+    }
+}
