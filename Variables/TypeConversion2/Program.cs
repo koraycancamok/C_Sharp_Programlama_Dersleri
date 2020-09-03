98cf67d289d98b4f7d5f3417fb50adbf172f322e
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:32:51 +0300

    TypeConversion2

---
+++ Variables/TypeConversion2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TypeConversion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var a = "50";
+            //var b = "100";
+            //int sonuc = int.Parse(a) + int.Parse(b);
+            //Console.WriteLine(sonuc);
+
+            //Console.Write("1. sayıyı gir");
+            //var sayi1 = Console.ReadLine();
+            //Console.Write("2.sayıyı gir");
+            //var sayi2 = Console.ReadLine();
+            //var sonuc = sayi1 + sayi2;
+            //Console.WriteLine(sonuc);
+
+            //Console.Write("1. sayıyı gir");
+            //var sayi1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("2.sayıyı gir");
+            //var sayi2 = Convert.ToInt32(Console.ReadLine());
+            //var sonuc = sayi1 + sayi2;
+            //Console.WriteLine(sonuc);
+
+            //byte a = 255;
+            //byte b = 10;
+            //var sonuc = a + b;
+            //Console.WriteLine(sonuc);
+
+            //int a = 10;
+            //int b = 3;
+            //int sonuc = a / b;
+            //Console.WriteLine(sonuc);
+
+            //int a = 10;
+            //int b = 3;
+            //float sonuc = (float)a / (float)b;
+            //Console.WriteLine(sonuc);
+
+            //string isWorking = "true";
+            //if (bool.Parse(isWorking))
+            //{
+            //    Console.WriteLine("if bloğu işletildi."); ;
+            //}
+            //else
+            //{
+            //    Console.WriteLine("if bloğu işletilmedi.");
+            //}
+        }
+    }
+}
