afd835b56507f1727b23404aa0997e95129c3f43
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:20:04 +0300

    WhileLoop

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 10;
+            //while (i >= 1)
+            //{
+            //    Console.WriteLine(i);
+            //    System.Threading.Thread.Sleep(1000);
+            //    i--;
+            //}
+
+            //int toplam = 0;
+            //char secim = 'e';
+            //int sayac = 1;
+            //while (secim == 'e')
+            //{
+            //    Console.Write("{0}. sayı giriniz : ", sayac);
+            //    toplam += int.Parse(Console.ReadLine());
+            //    Console.Write("devam etmek istiyor musun ? (e/h)");
+            //    secim = Convert.ToChar(Console.ReadLine());
+            //    sayac++;
+            //}
+            //Console.WriteLine("{1} adet sayıların toplamı : {0}", toplam, sayac-1);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhileLoop/WhileLoop2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace WhileLoop2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            char secim = 'e';
+            int hak = 5;
+            int sayac = 0;
+            int puan = 100;
+            int tutulan = (new Random()).Next(1, 10);
+            while (hak > 0)
+            {
+                sayac++;
+                Console.Write("sayı giriniz : ");
+                sayi = int.Parse(Console.ReadLine());
+                if (tutulan == sayi)
+                {
+                    Console.WriteLine("tebrik ederim {0}. defada bildiniz. puanınız {1}", sayac, puan - (sayac * 20));
+                    Console.Write("devam etmek istiyor musunuz? : ");
+                    secim = char.Parse(Console.ReadLine());
+                    if (secim == 'e')
+                    {
+                        hak = 6;
+                        sayac = 0;
+                        tutulan = (new Random()).Next(1, 10);
+                    }
+                    else
+                    {
+                        hak = -1;
+                    }
+                }
+                else
+                {
+                    if (tutulan > sayi)
+                    {
+                        Console.WriteLine("yukarı");
+                    }
+                    else
+                    {
+                        Console.WriteLine("aşağı");
+                    }
+                }
+                hak--;
+                if (hak == 0)
+                {
+                    Console.WriteLine("oyun bitti");
+                    Console.Write("devam etmek istiyor musunuz? : ");
+                    secim = char.Parse(Console.ReadLine());
+                    if (secim == 'e')
+                    {
+                        hak = 5;
+                        tutulan = (new Random()).Next(1, 10);
+                    }
+                    else
+                    {
+                        hak = -1;
+                    }
+                }        
+            }
+        }
+    }
+}
