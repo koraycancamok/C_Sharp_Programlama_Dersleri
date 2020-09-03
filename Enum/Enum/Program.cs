99b651f2e6968d4692480b71b4c5a722fc0b71cb
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 11:54:12 +0300

    Enum

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Enum
+{
+    class Program
+    {
+        public enum OdemeYontemleri : byte
+        {
+            KrediKarti = 1,
+            Havale = 2,
+            KapidaOdeme = 3,
+        }
+        static void Main(string[] args)
+        {
+            //var kredikartı = OdemeYontemleri.KrediKarti;
+            //var odemeid = (int)kredikartı;
+            //Console.WriteLine((OdemeYontemleri)odemeid);
+            //Console.WriteLine(odemeid);
+            //Console.WriteLine(kredikartı);
+
+            //string secim = " ";
+            //do
+            //{     
+            //    Console.Write("ödeme tipini giriniz (Kredi 1, Havale 2, Kapıda 3) : ");
+            //    var odemetipi = byte.Parse(Console.ReadLine());   
+            //    if (odemetipi < 4)
+            //    {
+            //        switch (odemetipi)
+            //        {
+            //            case (byte)OdemeYontemleri.KrediKarti:
+            //                Console.WriteLine("yapılan ödeme {0}", OdemeYontemleri.KrediKarti);
+            //                break;
+            //            case (byte)OdemeYontemleri.Havale:
+            //                Console.WriteLine("yapılan ödeme {0}", OdemeYontemleri.Havale);
+            //                break;
+            //            case (byte)OdemeYontemleri.KapidaOdeme:
+            //                Console.WriteLine("yapılan ödeme {0}", OdemeYontemleri.KapidaOdeme);
+            //                break;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("yanlış ödeme tipi");
+            //    }
+            //    Console.Write("devam ");
+            //    secim = Console.ReadLine();
+            //} while (secim == "e");
+            //Console.WriteLine("bitti");
+
+            Console.ReadLine();
+        }
+    }
+}
