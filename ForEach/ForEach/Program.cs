e2fa827b07618cf35e644db27e58a9c254e575ff
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 11:55:01 +0300

    ForEach

---
+++ ForEach/ForEach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string ad = "koraycan";
+            //for (int i = 0; i < ad.Length; i++)
+            //{
+            //    Console.WriteLine(ad[i]);
+            //}
+
+            int[] sayilar = { 10, 12, 2, 5, 7, 67, 56 };
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine(sayilar[i]);
+            }
+            Console.WriteLine("---------------");
+            foreach (var sayi in sayilar)
+            {
+                Console.WriteLine(sayi);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
