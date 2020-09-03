4be25e1cd48dc5641c4d8913c6dda3528474db90
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:27:10 +0300

    AssignmentOperators2

---
+++ Variables/AssignmentOperators2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace AssignmentOperators2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 50;
+            //int b;
+            //Console.Write("b değeri : ");
+            //string num1 = Console.ReadLine();
+            //bool sonuc = int.TryParse(num1, out b);
+            //if (!sonuc)
+            //{
+            //    Console.WriteLine("yanlış değer girdiniz");
+            //}
+            //else
+            //{
+            //    a += b;
+            //}
+            //Console.WriteLine(a);
+
+            //Console.Write("1. değeri giriniz : ");
+            //string deger1 = Console.ReadLine();
+            //Console.Write("2. değeri giriniz : ");
+            //string deger2 = Console.ReadLine();
+            //int sayi1, sayi2;
+            //bool sonuc = int.TryParse(deger1, out sayi1) & int.TryParse(deger2, out sayi2);
+            //if (sonuc)
+            //{
+            //    Console.WriteLine(sayi1 + sayi2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(deger1 + deger2);
+            //}
+
+            //Console.Write("bir sayı giriniz : ");
+            //int sayi = int.Parse(Console.ReadLine());
+            //if ((sayi % 5) == 0 && (sayi % 3) == 0)
+            //{
+            //    Console.WriteLine("15'e bölünebilir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("15'e bölünemez");
+            //}
+
+            //int toplam = 0;
+            //for (int i = 1; i < 100; i++)
+            //{
+            //    toplam = toplam + i;   
+            //}
+            //Console.WriteLine(toplam);
+
+            Console.ReadLine();
+        }
+    }
+}
