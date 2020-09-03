a1fd9939ab7979e245e84b956718e164fc53d0c6
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:08:43 +0300

    Struct_Class

---
+++ Struct_Class/Struct_Class/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Struct_Class
+{
+    class Program
+    {
+        class Urun
+        {
+            public string UrunAdi;
+            public float Fiyat;
+        }
+        static void Main(string[] args)
+        {
+            Urun iphone = new Urun();
+            iphone.UrunAdi = "Iphone 6S";
+            iphone.Fiyat = 2500;
+            UrunGetir(iphone);
+            UrunGuncelle(iphone);
+            UrunGetir(iphone);
+        }
+        static void UrunGetir(Urun urun)
+        {
+            Console.WriteLine($"urun adı : {urun.UrunAdi}, fiyat : {urun.Fiyat}");
+        }
+        static void UrunGuncelle(Urun urun)
+        { 
+            urun.Fiyat *= 1.5f; 
+        }
+    }
+}
