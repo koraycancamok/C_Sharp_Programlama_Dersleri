095cf00ec300c7e665ed1230676b4206492e6534
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:30:10 +0300

    LogicalOperators2

---
+++ Variables/LogicalOperators2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace LogicalOperators2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int yas = 17;
+            //bool veli_izni = false;
+            //if ((yas >= 18) || (veli_izni == true))
+            //{
+            //    Console.WriteLine("çalışabilir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("çalışamaz");
+            //}
+
+            //int not = 50;
+            //bool kosul1 = (not >= 45);
+            //bool kosul2 = (not <= 100);
+            //if (kosul1 & kosul2)
+            //{
+            //    Console.WriteLine("dersten geçti");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("dersten geçemedi");
+            //}
+
+            //int ort = 65;
+            //int zayif = 0;
+            //if ((ort >= 70) & (zayif == 0))
+            //{
+            //    Console.WriteLine("teşekkür belgesi alır");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("teşekkür belgesi alamaz");
+            //}
+
+            //string egitimseviyesi = "yüksekokul";
+            //bool isSigara = true;
+            //if ((egitimseviyesi == "yüksekokul" || egitimseviyesi == "üniversite") & (!isSigara))
+            //{
+            //    Console.WriteLine("işe alınabilir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("işe alınamaz");
+            //}
+
+            //string username = "koraycan";
+            //string email = "koray@gmail.com";
+            //string password = "12345";
+            //Console.Write("username yada email giriniz : ");
+            //string girilenusername = Console.ReadLine();
+            //Console.Write("parola girin : ");
+            //string girilenparola = Console.ReadLine();
+            //if ((username == girilenusername || girilenusername == email) & (girilenparola == password))
+            //{
+            //    Console.WriteLine("giriş yapıldı");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("kullanıcı adı veya parola yanlış");
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
