cefeb89fe7d0d1941faf9640b13ec805332538db
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:28:32 +0300

    CompareOperators2

---
+++ Variables/CompareOperators2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace CompareOperators2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int yas = 10;
+            //if (yas >= 18)
+            //{
+            //    Console.WriteLine("oy kullanabilirsiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("oy kullanamazsınız. {0} yılınız kaldı", 18 -yas);
+            //}
+
+            //Console.Write("sayı giriniz : ");
+            //int sayi = int.Parse(Console.ReadLine());
+            //string sonuc = sayi < 0 ? "sayı negatif" : "sayı pozitif";
+            //Console.WriteLine(sonuc);
+            //if (sayi < 0)
+            //{
+            //    Console.WriteLine("sayı negatif");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("sayı pozitif");
+            //}
+
+            //Console.Write("1. sayı : ");
+            //int sayi1 = int.Parse(Console.ReadLine());
+            //Console.Write("2. sayı : ");
+            //int sayi2 = int.Parse(Console.ReadLine());
+            //bool sonuc = (sayi1 == sayi2);
+            //string mesaj = sonuc ? "iki sayı eşit" : "iki sayı eşit değil";
+            //Console.WriteLine(mesaj);
+
+            //int a = 20, b = 10, c = 15;
+            //int max = c;
+            //if (a > max)
+            //{
+            //    max = a;
+            //}
+            //if (b > max)
+            //{
+            //    max = b;
+            //}
+            //Console.WriteLine("en büyük sayı : {0}", max);
+        }
+    }
+}
