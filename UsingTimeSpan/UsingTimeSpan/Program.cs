0d72bac9405f477a4c00a51889cb4a7914deec9d
Koray Can Çamok <32061801+koraycancamok@users.noreply.github.com>
2020-09-03 12:17:44 +0300

    UsingTimeSpan

---
+++ UsingTimeSpan/UsingTimeSpan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UsingTimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan ts1 = new TimeSpan(2, 10, 50);
+            TimeSpan ts2 = new TimeSpan(1, 0, 0);
+            TimeSpan ts3 = TimeSpan.FromHours(1);
+            Console.WriteLine("saniye : {0}",ts1.Seconds);
+            Console.WriteLine("dakika : {0}",ts1.Minutes);
+            Console.WriteLine("saat : {0}",ts1.Hours);
+            Console.WriteLine("toplam dakika : {0}",ts1.TotalMinutes);
+
+            //DateTime now1 = DateTime.Now;
+            //DateTime now2 = DateTime.Now.AddHours(2);
+            //TimeSpan timespan = now2 - now1;
+            //Console.WriteLine("geçen süre : {0}",timespan.TotalMilliseconds);
+        }
+    }
+}
